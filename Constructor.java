4ccf42370b0410523c0870cb7102a1a431c1fc07
appu22 <50874934+appu22@users.noreply.github.com>
2019-10-04 12:08:15 +0530

    Constructor
    Write a Java program that creates an object and initializes its data members using constructor. Use constructor overloading concept.

---
+++ Constructor.java
@@ -0,0 +1,23 @@
+package constructor;
+public class Constructor 
+{
+        static int n1=0,n2=1,n3=0;
+        static void printfibo(int count)
+        {
+            if(count>0)
+            {
+                n3=n1+n2;
+                n1= n2;
+                n2=n3;
+                System.out.println(""+n3);
+                printfibo(count-1);
+            }
+        }
+    public static void main(String[] args) 
+    {
+        int count=5;
+        System.out.println(n1);
+        System.out.println(n2);
+        printfibo(count-2);
+    }
+}
